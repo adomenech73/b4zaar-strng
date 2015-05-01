c59ff54154a79fc9c13482df4200d7249071809a
albert <adomenech@gmail.com>
2015-05-01 02:35:02 +0200

    Near product method

--- common/models/product.js
+++ common/models/product.js
@@ -1,3 +1,13 @@
 module.exports = function (Product) {
 
+  Product.Near = function (here, cb) {
+    Product.find({where: {loc: {near: here}}, limit: 3}, function (err, nearbyProducts) {
+      console.info(nearbyProducts); // [Products, ...]
+    })
+  };
+  Product.remoteMethod('Near', {
+    accepts: {arg: 'here', type: 'GeoPoint'},
+    returns: {arg: 'prods', type: 'String'}
+  });
+
 };
--- deploy/Docker/build.sh
+++ deploy/Docker/build.sh
@@ -3,7 +3,7 @@
 for dir in `ls -d */`;
 do
   cd ./$dir
-  docker build -t adomenech73/${dir%%/} .
+  docker build --force-rm=true --no-cache=true -t adomenech73/${dir%%/} .
   cd ../
 done
 
